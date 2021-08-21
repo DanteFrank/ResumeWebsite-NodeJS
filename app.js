75050f7ed7ec4b0907b7c9b2472cbea6d756271d
DanteFrank <davidfrankoziwo@gmail.com>
2021-08-21 22:24:59 +0100

    Inital Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const nodemailer = require("nodemailer");
+require("dotenv/config");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//       res.send("Voke is seriously a pain my ass");
+// });
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "yahoomail",
+    auth: {
+      user: "davidfrankoziwo@yahoo.com",
+      password: process.env.YAHOO_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "davidfrankoziwo@yahoo.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Success");
+    }
+  });
+});
+
+app.listen(PORT, console.log(`Server is running successfuly on port ${PORT}`));
---
+++ public/js/contactForm.js
@@ -0,0 +1,35 @@
+const contactForm = document.querySelector(".contact-form");
+
+const thename = document.getElementById("thename");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = {
+    name: thename.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = () => {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email Sent Successfuly");
+      thename.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong. Please try again.");
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
+});
